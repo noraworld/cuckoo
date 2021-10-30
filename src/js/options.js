07101df10a19793c1b8b13a390e7f19144d7876a
noraworld <mail@noraworld.com>
2021-10-30 16:48:37 +0900

    Support i18n

--- src/js/options.js
+++ src/js/options.js
@@ -30,8 +30,30 @@ function eventListener() {
   })
 }
 
+// https://stackoverflow.com/questions/25467009/internationalization-of-html-pages-for-my-google-chrome-extension#answer-25612056
+function localize() {
+    //Localize by replacing __MSG_***__ meta tags
+    var objects = document.getElementsByTagName('html');
+    for (var j = 0; j < objects.length; j++)
+    {
+        var obj = objects[j];
+
+        var valStrH = obj.innerHTML.toString();
+        var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1)
+        {
+            return v1 ? chrome.i18n.getMessage(v1) : "";
+        });
+
+        if(valNewH != valStrH)
+        {
+            obj.innerHTML = valNewH;
+        }
+    }
+}
+
 (function() {
   window.addEventListener('DOMContentLoaded', () => {
+    localize() // must be loaded at first because if not so, setting event listener will not work
     eventListener()
     restore()
   })
